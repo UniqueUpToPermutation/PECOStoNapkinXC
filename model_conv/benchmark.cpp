7206cc370416b24e57d05ad7d0b3a89ed78afbbe
UniqueUpToPermutation <paetter@stanford.edu>
2021-08-08 18:29:48 -0400

    pecos-to-napkinXC

---
+++ model_conv/benchmark.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+
+#include <models/tree.h>
+#include <models/plt.h>
+
+#include <ctime>
+#include <set>
+#include <filesystem>
+
+#include <pecos/core/xmc/inference.hpp>
+#include <pecos/core/utils/scipy_loader.hpp>
+
+pecos::csr_t OnlyFirstRow(pecos::csr_t mat) {
+	pecos::csr_t out;
+
+	auto nnz = mat.indptr[1];
+
+	out.allocate(1, mat.cols, nnz);
+
+	std::memcpy(out.indices, mat.indices, nnz * sizeof(pecos::csr_t::index_type));
+	std::memcpy(out.val, mat.val, nnz * sizeof(pecos::csr_t::value_type));
+	std::memcpy(out.indptr, mat.indptr, 2 * sizeof(pecos::csr_t::mem_index_type));
+
+	return out;
+}
+
+SRMatrix<Feature> PecosToNapkinXC(const pecos::csr_t& mat, double bias) {
+	SRMatrix<Feature> m;
+	
+	for (int row = 0; row < mat.rows; ++row) {
+
+		auto start = mat.indptr[row];
+		auto end = mat.indptr[row+1];
+		auto len = end - start;
+
+		std::vector<Feature> row_features;
+		row_features.reserve(len);
+
+		for (auto i = start; i < end; ++i) {
+			Feature f;
+			f.index = mat.indices[i];
+			f.value = mat.val[i];
+			row_features.emplace_back(f);
+		}
+
+		if (bias > 0.0) {
+			Feature f;
+			f.index = mat.cols;
+			f.value = bias;
+			row_features.emplace_back(f);
+		}
+
+		m.appendRow(row_features);
+	}
+
+	return m;
+}
+
+std::vector<std::vector<Prediction>> PecosPredictionToNapkinXC(const pecos::csr_t& mat) {
+	std::vector<std::vector<Prediction>> result;
+	result.reserve(mat.rows);
+	
+	for (int row = 0; row < mat.rows; ++row) {
+
+		auto start = mat.indptr[row];
+		auto end = mat.indptr[row+1];
+		auto len = end - start;
+
+		std::vector<Prediction> row_vec;
+		row_vec.reserve(len);
+
+		for (auto i = start; i < end; ++i) {
+			Prediction p;
+			p.label = mat.indices[i];
+			p.value = mat.val[i];
+			row_vec.emplace_back(p);
+		}
+
+		result.emplace_back(std::move(row_vec));
+	}
+
+	return result;
+}
+
+void ComputeRecallPrecision(
+	const std::vector<std::vector<Prediction>>& ground_truth, 
+	const std::vector<std::vector<Prediction>>& predictions,
+	int topK,
+	std::vector<double>& recall,
+	std::vector<double>& precision) {
+
+	recall.resize(topK);
+	precision.resize(topK);
+
+	std::fill(recall.begin(), recall.end(), 0.0);
+	std::fill(precision.begin(), precision.end(), 0.0);
+
+	for (int i = 0; i < ground_truth.size(); ++i) {
+		auto& truth = ground_truth[i];
+		auto& prediction = predictions[i];
+
+		std::set<int> truth_labels;
+
+		for (auto& t : truth) {
+			truth_labels.emplace(t.label);
+		}
+
+		for (int k = 1; k <= topK; ++k) {
+			std::set<int> pred_labels;
+			std::set<int> pred_truth_labels;
+
+			for (int j = 0; j < k; ++j) {
+				pred_labels.emplace(prediction[j].label);
+			}
+
+			std::set_intersection(truth_labels.begin(), truth_labels.end(), 
+				pred_labels.begin(), pred_labels.end(),
+				std::inserter(pred_truth_labels, pred_truth_labels.begin()));
+
+			recall[k-1] += (double)pred_truth_labels.size() / (double)truth_labels.size();
+			precision[k-1] += (double)pred_truth_labels.size() / (double)pred_labels.size();
+		}
+	}
+
+	for (auto& r : recall) {
+		r /= (double)ground_truth.size();
+	}
+
+	for (auto& p : precision) {
+		p /= (double)ground_truth.size();
+	}
+}
+
+void TestDataSet(const std::filesystem::path& path) {
+
+	// Verify that we have both a napkin and pecos model
+	auto pecos_path = path / "model" / "ranker";
+	auto napkin_path = path / "napkin-model";
+
+	if (!std::filesystem::exists(pecos_path) || !std::filesystem::is_directory(pecos_path)) {
+		std::cout << path << " does not have a PECOS model. Skipping..." << std::endl;
+		return;
+	}
+
+	if (!std::filesystem::exists(napkin_path) || !std::filesystem::is_directory(napkin_path)) {
+		std::cout << path << " does not have a Napkin-XC model. Skipping..." << std::endl;
+		return;
+	}
+
+	pecos::csr_t X;
+	pecos::csr_t Y;
+
+	std::vector<std::vector<Prediction>> pecos_predictions;
+	std::vector<std::vector<Prediction>> napkin_predictions;
+
+	int top_k = 10;
+	int beam_size = 20; 
+
+	{
+		std::cout << "Loading " << path / "X.tst.tfidf.npz" << "..." << std::endl;
+		pecos::ScipyCsrF32Npz X_npz(path / "X.tst.tfidf.npz");
+		std::cout << "Loading " << path / "Y.tst.npz" << "..." << std::endl;
+		pecos::ScipyCsrF32Npz Y_npz(path / "Y.tst.npz");
+		X = pecos::csr_npz_to_csr_t_deep_copy(X_npz);
+		Y = pecos::csr_npz_to_csr_t_deep_copy(Y_npz);
+	}
+
+	{
+		std::cout << "Loading PECOS model " << pecos_path << "..." << std::endl;
+		pecos::HierarchicalMLModel model(pecos_path, pecos::LAYER_TYPE_HASH_CHUNKED);
+
+		std::cout << "Running PECOS Prediction..." << std::endl;
+		pecos::csr_t Y_pred;
+
+		std::clock_t c_start = std::clock();
+		model.predict<pecos::csr_t, pecos::csr_t>(X, Y_pred, beam_size, "sigmoid", top_k, 1);
+		std::clock_t c_end = std::clock();
+
+		long double time_elapsed_ms = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+		std::cout << "CPU time per query: " << time_elapsed_ms / (double)X.rows << " ms\n";
+
+		pecos_predictions = PecosPredictionToNapkinXC(Y_pred);
+		Y_pred.free_underlying_memory();
+
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "Loading NapkinXC model " << napkin_path << "..." << std::endl;
+		auto current_dir = std::filesystem::current_path();
+
+		std::filesystem::current_path(napkin_path);
+
+		Args args;
+		args.loadFromFile("args.bin");
+
+		BatchPLT model_;
+		model_.load(args, args.output);
+
+		SRMatrix<Feature> X_f = PecosToNapkinXC(X, 1.0);
+
+		std::cout << "Running NapkinXC Prediction..." << std::endl;
+
+		args.topK = top_k;
+		args.beamSearchWidth = beam_size;
+		args.threads = 1;
+		args.treeSearchType = TreeSearchType::beam;
+
+		std::clock_t c_start = std::clock();
+		napkin_predictions = model_.predictBatch(X_f, args);
+		std::clock_t c_end = std::clock();
+
+		for (auto& pred : napkin_predictions) {
+			pred.resize(std::min<int>(pred.size(), args.topK));
+		}
+
+		long double time_elapsed_ms = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+		std::cout << "CPU time per query: " << time_elapsed_ms  / (double)X.rows << " ms\n";
+
+		std::filesystem::current_path(current_dir);
+
+		std::cout << std::endl;
+	}
+
+	std::vector<double> pecos_recall;
+	std::vector<double> pecos_precision;
+
+	std::vector<double> napkin_recall;
+	std::vector<double> napkin_precision;
+
+	auto truth = PecosPredictionToNapkinXC(Y);
+
+	ComputeRecallPrecision(truth, pecos_predictions, top_k, pecos_recall, pecos_precision);
+	ComputeRecallPrecision(truth, napkin_predictions, top_k, napkin_recall, napkin_precision);
+
+	auto printPrecisionRecall = [top_k](const std::string& header, 
+		const std::vector<double>& precision,
+		const std::vector<double>& recall) {
+		std::cout << "=========== " << header << " =============" << std::endl;
+		std::cout << std::setw(10) << "prec@k";
+		for (int i = 0; i < top_k; ++i) {
+			std::cout << std::setw(10) << precision[i];
+		}
+		std::cout << std::endl;
+		std::cout << std::setw(10) << "recall@k";
+		for (int i = 0; i < top_k; ++i) {
+			std::cout << std::setw(10) << recall[i];
+		}
+		std::cout << std::endl << std::endl;
+	};
+
+	printPrecisionRecall("PECOS", pecos_precision, pecos_recall);
+	printPrecisionRecall("NapkinXC", napkin_precision, napkin_recall);
+
+	X.free_underlying_memory();
+	Y.free_underlying_memory();
+}
+
+int main() {
+	auto path = std::filesystem::path(DATA_DIR);
+
+	for (auto entry : std::filesystem::directory_iterator(path)) {
+		if (entry.is_directory()) {
+			TestDataSet(entry.path());
+		}
+	}
+}
---
+++ model_conv/conv.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+
+#include <models/tree.h>
+#include <models/plt.h>
+
+#include <filesystem>
+
+#include <pecos/core/xmc/inference.hpp>
+
+struct NodeClassRanges {
+	int _start;
+	int _end;
+};
+
+// Build the tree
+Tree* makeTreeFrom(pecos::HierarchicalMLModel& model) {
+	Tree* tree = new Tree();
+	TreeNode* root = tree->createTreeNode();
+	tree->root = root;
+
+	int base_index = 0;
+
+	root->index = base_index++;
+	root->parent = nullptr;
+
+	std::vector<TreeNode*> lastLayer = { root };
+	std::vector<TreeNode*> nextLayer;
+
+	for (auto layer : model.get_model_layers()) {
+		auto layer_cast = dynamic_cast<pecos::MLModel<pecos::csc_t>*>(layer);
+		auto& layer_data = layer_cast->get_layer_data();
+
+		auto nextLayerSize = layer_data.C.rows;
+		for (int i = 0; i < nextLayerSize; ++i) {
+			auto node = tree->createTreeNode();
+			node->index = base_index++;
+			nextLayer.emplace_back(node);
+		}
+
+		for (int parent = 0; parent < lastLayer.size(); ++parent) {
+			auto start = layer_data.C.indptr[parent];
+			auto end = layer_data.C.indptr[parent+1];
+			auto parentNode = lastLayer[parent];
+
+			for (auto idx = start; idx < end; ++idx) {
+				auto child_idx = layer_data.C.indices[idx];
+				auto childNode = nextLayer[child_idx];
+
+				tree->setParent(childNode, parentNode);
+			}
+		}
+
+		lastLayer = std::move(nextLayer);
+		nextLayer.clear();
+	}
+
+	int current_label = 0;
+	for (auto node : lastLayer) {
+		tree->setLabel(node, current_label++);
+	}
+
+	for (int nodeId = tree->nodes.size() - 1; nodeId >= 0; --nodeId) {
+		auto node = tree->nodes[nodeId];
+		auto& children = node->children;
+
+		if (children.size() == 0) {
+			node->subtreeLeaves = 1;
+
+		} else {
+			int subtreeLeaves = 0;
+			for (auto& child : children) {
+				subtreeLeaves += child->subtreeLeaves;
+			}
+			node->subtreeLeaves = subtreeLeaves;
+		}
+	}
+
+	tree->k = tree->leaves.size();
+	tree->t = tree->nodes.size();
+
+	return tree;
+}
+
+// Make the weight vectors
+std::vector<Base*> makeBasesFrom(pecos::HierarchicalMLModel& model) {
+	std::vector<Base*> result;
+
+	Base* b = new Base();
+	b->W = nullptr;
+	b->firstClass = 0;
+	b->classCount = 0;
+	b->lossType = LossType::logistic;
+
+	result.emplace_back(b);
+
+	for (auto layer : model.get_model_layers()) {
+		auto layer_cast = dynamic_cast<pecos::MLModel<pecos::csc_t>*>(layer);
+		auto& layer_data = layer_cast->get_layer_data();
+		auto& weights = layer_data.W;
+
+		for (int i = 0; i < weights.cols; ++i) {
+
+			auto start = weights.indptr[i];
+			auto end = weights.indptr[i+1];
+			auto len = end - start;
+
+			auto vec = new MapVector<Weight>(len);
+
+			for (auto idx = start; idx < end; ++idx) {
+				vec->insertD(weights.indices[idx], weights.data[idx]);
+			}
+
+			Base* b = new Base();
+			b->W = vec;
+			b->firstClass = 1; // WTF?
+			b->classCount = 2; // WTF?
+			b->lossType = LossType::logistic;
+
+			result.emplace_back(b);
+		}
+	}
+
+	return result;
+}
+
+void ConvertModel(std::filesystem::path path, std::filesystem::path out_path) {
+	std::filesystem::path _model_dir_in = path;
+	std::filesystem::path _model_dir_out = out_path;
+
+	{
+		std::cout << "Loading PECOS model from " << _model_dir_in << "..." << std::endl;
+
+		pecos::HierarchicalMLModel model(_model_dir_in, pecos::LAYER_TYPE_CSC);
+
+		Tree* tree = makeTreeFrom(model);
+		std::vector<Base*> bases = makeBasesFrom(model);
+
+		if (!std::filesystem::exists(_model_dir_out)) {
+			std::filesystem::create_directory(_model_dir_out);
+		} 
+		
+		std::ofstream _os_bases;
+		std::ofstream _os_tree;
+		std::ofstream _os_args;
+		
+		if (std::filesystem::is_directory(_model_dir_out)) {
+
+			std::filesystem::path _bases_out = _model_dir_out / "weights.bin";
+			std::filesystem::path _tree_out = _model_dir_out / "tree.bin";
+			std::filesystem::path _args_out = _model_dir_out / "args.bin";
+
+			_os_bases = std::ofstream(_bases_out);
+			_os_tree = std::ofstream(_tree_out);
+			_os_args = std::ofstream(_args_out);
+
+		} else {
+			throw std::runtime_error("Output directory exists and is not a directory!");
+		}
+
+		std::cout << "Saving NapkinXC model to " << _model_dir_out << "..." << std::endl;
+
+		int size = bases.size();
+		_os_bases.write((char*)&size, sizeof(size));
+		for (int i = 0; i < bases.size(); ++i) {
+			Base* base = bases[i];
+			base->save(_os_bases, false);
+			delete base;
+		}
+		_os_bases.close();
+
+		tree->save(_os_tree);
+		delete tree;
+		_os_tree.close();
+
+		std::cout << "Verifying that NapkinXC can load model..." << std::endl;
+
+		Args args;
+		args.modelType = ModelType::plt;
+
+		args.save(_os_args);
+		_os_args.close();
+	}
+
+	{
+		auto current_dir = std::filesystem::current_path();
+
+		std::filesystem::current_path(_model_dir_out);
+
+		Args args;
+		args.loadFromFile(_model_dir_out / "args.bin");
+       	auto model_ = Model::factory(args);
+
+        if (!model_->isLoaded()) 
+			model_->load(args, args.output);
+		else 
+			throw std::runtime_error("Cannot load!");
+
+		std::filesystem::current_path(current_dir);
+
+		std::cout << "Verification successful!" << std::endl;	
+	}
+}
+
+int main(int argc, char *argv[]) {
+
+	std::vector<std::filesystem::path> model_dirs;
+
+	if (argc < 2) {
+		std::filesystem::path data_dir = DATA_DIR;
+		model_dirs.emplace_back(data_dir / "eurlex-4k");
+		model_dirs.emplace_back(data_dir / "amazoncat-13k");
+		model_dirs.emplace_back(data_dir / "wiki10-31k");
+		model_dirs.emplace_back(data_dir / "wiki-500k");
+	} else {
+		for (int i = 1; i < argc; ++i) {
+			model_dirs.emplace_back(argv[i]);
+		}
+	}
+
+	for (auto dir_entry : model_dirs) {
+		std::filesystem::path pecosModelPath = dir_entry / "model" / "ranker";
+		std::filesystem::path napkinXCModelPath = dir_entry / "napkin-model";
+		if (std::filesystem::exists(pecosModelPath)) {
+			std::cout << "Found PECOS model " << pecosModelPath << "..." << std::endl;
+			ConvertModel(pecosModelPath, napkinXCModelPath);
+		}
+	}
+}
